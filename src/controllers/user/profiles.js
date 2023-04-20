f8b04112fc346ba04c9f27f98a8faabeb81880cb
MarianaKely <marianakely.r@gmail.com>
2023-04-20 02:23:34 -0300

    user folder update

--- src/controllers/user/profiles.js
+++ src/controllers/user/profiles.js
@@ -8,18 +8,26 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL)
 let db;
 
 try {
+
   await mongoClient.connect()
   console.log("MongoDB Connected!");
   db = mongoClient.db()
+
 } catch (error) {
+
   console.log('Problemas no servidor')
 }
 
 export async function profiles (req, res) {
+
     try {
+
       const analysis = await db.collection("users").find().toArray()
       return res.send(analysis)
+
     } catch (error) {
+
       res.status(500).send("Server problems")
+      
     }
   }
