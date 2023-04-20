65d29d3695561fc87ed2c9e7c6bffc26a93eddfc
MarianaKely <marianakely.r@gmail.com>
2023-04-20 02:17:45 -0300

    middleware update

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -8,26 +8,34 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL)
 let db;
 
 try {
+
   await mongoClient.connect()
   console.log("MongoDB Connected!");
   db = mongoClient.db()
+
 } catch (error) {
+
   console.log('Problemas no servidor')
 }
 
 export async function middleware(req, res, next) {
+
     const { authorization } = req.headers;
     const internalAnalysis = authorization?.replace("Bearer ", "");
 
     if (!internalAnalysis) return res.status(422).send("Informe o token");
 
     try {
+
         const room = await db.collection("sessions").findOne({ token: internalAnalysis });
         if (!room) return res.status(422).send("Acesso negado");
         res.locals.session = room;
         next();
+
     } catch (err) {
+
         return res.status(500).send(err);
+        
     }
 
 }
