2ece0725696910450ae7de5566cfe3b0172165f8
MarianaKely <marianakely.r@gmail.com>
2023-04-20 02:24:09 -0300

    joi configs update

--- src/joi/registerSchema.js
+++ src/joi/registerSchema.js
@@ -2,8 +2,10 @@
 import joi from 'joi'
 
 export const registerSchema = joi.object({
+
     name: joi.string().required(),
     email: joi.string().email().required(),
     password: joi.string().required(),
     confirmPassword: joi.string().valid(joi.ref('password')).required()
+    
   });
--- src/joi/transferenceSchema.js
+++ src/joi/transferenceSchema.js
@@ -2,6 +2,8 @@
 import joi from 'joi'
 
 export const transferenceSchema = joi.object({
+
     value: joi.string().replace(',', '.', true).regex(/^\d+(\.\d{1,2})?$/).required(),
     description: joi.string().required(),
+    
   });
